61034acc381c4d823fafd803b2b49430dbfdfd41
matheusrcruz <git.dev@outlook.com>
2022-02-20 21:32:54 -0300

    fix: resolvendo problema de lista com spread operator, para mudar referencia de objeto

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -7,11 +7,13 @@ export class Negociacoes{
         this.negociacoes.push(negociacao);
     }
     lista(): Array<Negociacao>{
-        return this.negociacoes;
+        return [...this.negociacoes]; // Pegue cada item desse array e coloca nessa nova lista com o spread operator 
+                                      // Isso significa que não vai alterar a negociação original :) 
     }
+    
 }
 
-const neociacoes =  new Negociacoes();
-neociacoes.lista().forEach(n =>{
+// const neociacoes =  new Negociacoes();
+// neociacoes.lista().forEach(n =>{
    
-})
+// })
--- dist/js/models/negociacoes.js
+++ dist/js/models/negociacoes.js
@@ -6,9 +6,10 @@ export class Negociacoes {
         this.negociacoes.push(negociacao);
     }
     lista() {
-        return this.negociacoes;
+        return [...this.negociacoes]; // Pegue cada item desse array e coloca nessa nova lista com o spread operator 
+        // Isso significa que não vai alterar a negociação original :) 
     }
 }
-const neociacoes = new Negociacoes();
-neociacoes.lista().forEach(n => {
-});
+// const neociacoes =  new Negociacoes();
+// neociacoes.lista().forEach(n =>{
+// })
